bf7be26dd6ff131eecaf58769c2cc77fd83697bb
Krister <krister.torsvik@gmail.com>
2016-10-04 12:41:10 +0200

    wip: refactor

--- fibonacci.js
+++ fibonacci.js
@@ -2,17 +2,13 @@
 // write a node app that takes one number as the only parameter,
 // and it returns all the fibonacci sequence numbers
 // that are smaller than that number
-
+var fib = [];
 function fibonacciSequence(number) {
-
-  var fib = [];
-
-  fib[0] = 0;
-  fib[1] = 1;
-
   for(var i = 2; i <= 100; i++) {
+    fib[0] = 0;
+    fib[1] = 1;
     fib[i] = fib[i-2] + fib[i-1];
-    
+
     if(number <= fib[i]) {
       return;
     };
